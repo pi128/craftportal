a7f4d68b3766aee3e48061ddd623434444f4a5d8
pi128 <77074839+pi128@users.noreply.github.com>
2025-04-10 12:25:24 -0400

    sure

---
+++ BackFacing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BackFacing" tilewidth="32" tileheight="32" tilecount="90" columns="9">
+ <image source="SteveSprites/BackFacing.png" width="288" height="324"/>
+</tileset>
---
+++ roguelikeChar_transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="roguelikeChar_transparent" tilewidth="32" tileheight="32" tilecount="168" columns="28">
+ <image source="../../Downloads/kenney_roguelike-characters/Spritesheet/roguelikeChar_transparent.png" width="918" height="203"/>
+</tileset>
